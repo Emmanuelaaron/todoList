227616b5515fd7b55b704438e43554cea0933c67
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2021-05-10 18:06:45 +0300

    updates the Todo and Project functions to es6 syntax

---
+++ src/functions/todo.js
@@ -0,0 +1,10 @@
+class Todo {
+  constructor(name, description, dueDate, priority) {
+    this.name = name;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
+
+export default Todo;
