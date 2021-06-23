5f23c81b4c05cae8bfebb1978313445ccc0ae243
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2021-06-23 11:10:30 +0300

    removes comments from todo.js file

--- src/dom/todos.js
+++ src/dom/todos.js
@@ -33,8 +33,7 @@ const myTodos = (todos) => {
   todos.forEach((todo) => {
     const todoDiv = document.createElement('div');
     todoDiv.classList.add('border', 'pl-5', 'rounded', 'mb-3');
-    // todoDiv.id = `${todo.name}big`
-
+    
     const pro = document.createElement('div');
     pro.classList.add('text-decoration-none', 'text-dark');
     pro.innerHTML = `title:-       ${todo.name}`;
